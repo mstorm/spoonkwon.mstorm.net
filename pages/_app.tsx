981665e06e04134ee4d146557c384e22bcae055c
Minchul Kwon <mstorm@mstorm.net>
2022-07-02 17:17:50 +0900

    Add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,27 @@
 import '../styles/globals.css'
 import '../styles/slick-slider.css'
 import type { AppProps } from 'next/app'
+import Script from "next/script";
+
+const GA_MEASUREMENT_ID = 'G-EJR9X2H4D5'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* <!-- Global site tag (gtag.js) - Google Analytics -->*/}
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_MEASUREMENT_ID}');
+      `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
